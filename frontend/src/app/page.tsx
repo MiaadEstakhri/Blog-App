64c34c7c042d303f0f27f74c103215fcf564b989
MiaadEstakhri <miaad8104@gmail.com>
2025-07-16 20:30:18 +0330

    _

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./MTailwind";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-gray-500">
+      <Button color="blue" variant="gradient">
+        add
+      </Button>
+    </div>
+  );
+}
