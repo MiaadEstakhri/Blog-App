055a41e41f82039fe0de42250333eb79cf82c57e
MiaadEstakhri <miaad8104@gmail.com>
2025-07-19 11:08:06 +0330

    added font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,17 @@
 import type { Metadata } from "next";
-import "./styles/globals.css";
+import { Geist, Roboto } from "next/font/google";
+import vazirFont from "@constants/localFonts";
+import "../styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Blog App",
@@ -13,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
-      <body>{children}</body>
+      <body
+        className={`${vazirFont.variable} ${geistSans.variable} ${roboto.variable} font-sans`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
