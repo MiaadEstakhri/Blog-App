ae49826aeac03e12247ebcd7eb653286519110cd
MiaadEstakhri <miaad8104@gmail.com>
2025-07-21 17:12:31 +0330

    create layout for blogs

---
+++ frontend/src/app/blogs/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+type LayoutProps = {
+  children: ReactNode;
+};
+
+function layout({ children }: LayoutProps) {
+  return (
+    <div>
+      <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
+      <div className="grid grid-cols-12 gap-8">
+        <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
+          category
+        </div>
+        <div className="col-span-12 lg:col-span-8 xl:col-span-9">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default layout;
