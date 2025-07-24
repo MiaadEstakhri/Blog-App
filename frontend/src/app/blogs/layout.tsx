5f405e8d398fe5e13e2cfa790da0597c36d5d23f
MiaadEstakhri <miaad8104@gmail.com>
2025-07-24 16:28:35 +0330

    used categoryList

--- frontend/src/app/blogs/layout.tsx
+++ frontend/src/app/blogs/layout.tsx
@@ -1,15 +1,21 @@
+import { Metadata } from "next";
 import { ReactNode } from "react";
+import CategoryList from "./_components/CategoryList/CategoryList";
+
 type LayoutProps = {
   children: ReactNode;
 };
 
+export const metadata: Metadata = {
+  title: "blog",
+};
 function layout({ children }: LayoutProps) {
   return (
     <div>
       <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
-          category
+          <CategoryList />
         </div>
         <div className="col-span-12 lg:col-span-8 xl:col-span-9">
           {children}
