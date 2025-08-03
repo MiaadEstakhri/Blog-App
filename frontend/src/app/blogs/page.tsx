fceee9bb5bb39c7210a89a15ba7989bf7c4584e2
MiaadEstakhri <miaad8104@gmail.com>
2025-08-03 16:42:44 +0330

    Update blogs page

--- frontend/src/app/blogs/page.tsx
+++ frontend/src/app/blogs/page.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
 import React from "react";
 
-function BlogPage() {
-  return <div>BlogPage</div>;
+async function BlogPage() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  const { posts } = res.data.data;
+  return (
+    <div>
+      {posts?.map((item: any) => (
+        <div key={item._id}>{item.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default BlogPage;
