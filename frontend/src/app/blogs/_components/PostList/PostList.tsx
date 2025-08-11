e79ff4ed3507ca6fccbd0f95b1b4ae8ae9a6a14c
MiaadEstakhri <miaad8104@gmail.com>
2025-08-11 17:58:05 +0330

    feat(blogs/PostList): update PostList component

--- frontend/src/app/blogs/_components/PostList/PostList.tsx
+++ frontend/src/app/blogs/_components/PostList/PostList.tsx
@@ -1,7 +1,9 @@
-import axios from "axios";
 import React from "react";
+import Link from "next/link";
+import axios from "axios";
 import { PostListType } from "./postList.type";
-import { CoverImage } from "@components";
+import { ClockIcon } from "@heroicons/react/24/outline";
+import { Avatar, CoverImage } from "@ui";
 
 async function PostList() {
   await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -11,12 +13,33 @@ async function PostList() {
 
   return posts?.length > 0 ? (
     <div className="grid grid-cols-12 gap-8">
-      {posts?.map((item: PostListType) => (
+      {posts?.map((post: PostListType) => (
         <div
-          key={item?._id}
+          key={post?._id}
           className="grid col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-300 p-2 rounded-lg"
         >
-          <CoverImage src={item?.coverImageUrl} alt={item?.title} />
+          <CoverImage {...post} />
+          <div>
+            <Link href={`/blog/${post?.slug}`}>
+              <h2 className="mb-4 font-bold text-secondary-700">
+                {post?.title}
+              </h2>
+            </Link>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-x-1">
+                <Avatar src={post?.author?.avatarUrl} width={24} />
+                <span className="text-sm text-secondary-500">
+                  {post?.author?.name}
+                </span>
+              </div>
+              <div className="flex items-center text-[10px] text-secondary-500">
+                <ClockIcon className="w-4 h-4 stork-secondary-500 mx-1" />
+                <span className="">خواندن</span>
+                <span className="mx-1 leading-3">{post?.readingTime}</span>
+                <span className="">دقیقه</span>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
