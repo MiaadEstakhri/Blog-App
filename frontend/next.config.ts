3d6ab072872088463175252efef313024e49f389
MiaadEstakhri <miaad8104@gmail.com>
2025-08-11 17:58:04 +0330

    chore(next): update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("http://localhost:5005/uploads/coverImage/**")],
+    remotePatterns: [new URL("http://localhost:5005/uploads/**")],
   },
 };
 
