4e04bd447a31cd378ac6b1183de077edb2bb91d5
MiaadEstakhri <miaad8104@gmail.com>
2025-08-11 17:58:05 +0330

    types(blogs/PostList): update PostListType

--- frontend/src/app/blogs/_components/PostList/postList.type.ts
+++ frontend/src/app/blogs/_components/PostList/postList.type.ts
@@ -2,4 +2,10 @@ export type PostListType = {
   _id: string;
   title: string;
   coverImageUrl: string;
+  slug: string;
+  readingTime: number;
+  author: {
+    avatarUrl: string;
+    name: string;
+  };
 };
