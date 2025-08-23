4820bd349df32147ed7cb2ad60e5b692d995fe6a
MiaadEstakhri <miaad8104@gmail.com>
2025-08-23 17:21:21 +0330

    Add ButtonIcon component with TypeScript fixes

---
+++ frontend/src/ui/ButtonIcon/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+type BtnTypeProps = {
+  primary?: string;
+  secondary?: string;
+  outline?: string;
+  red?: string;
+  danger?: string;
+};
+
+const btnType: BtnTypeProps = {
+  primary:
+    "bg-primary-100 text-primary-700 hover:bg-primary-900 hover:text-white",
+  secondary:
+    "bg-secondary-200 text-secondary-500 hover:bg-secondary-500 hover:text-secondary-0",
+  outline:
+    "border border-secondary-200 text-secondary-500 hover:bg-secondary-200",
+  red: "bg-red-100 text-red-500 hover:bg-red-500 hover:text-white",
+  danger: "border border-red-100 text-red-500",
+};
+
+type VariantProps = "primary" | "secondary" | "outline" | "red" | "danger";
+
+type ButtonIconProps = {
+  className: string;
+  variant: VariantProps;
+  children: React.ReactNode;
+  onClick: () => void;
+};
+
+export function ButtonIcon({
+  className,
+  variant,
+  children,
+  onClick,
+  ...rest
+}: ButtonIconProps) {
+  return (
+    <button
+      className={`${btnType[variant]} ${className} flex items-center justify-center gap-x-1 rounded-md p-1 
+        [&>svg]:w-5 [&>svg]:h-5 [&>svg]:text-inherit text-xs lg:text-sm
+         transition-all duration-300 ease-out `}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
